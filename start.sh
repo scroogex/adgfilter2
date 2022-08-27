99a951b58da7e353f541745a11cc3cd378a0fcf3
scroogex <111561404+scroogex@users.noreply.github.com>
2022-08-27 11:38:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/scroogex/adgsearch /adgsearch
+  git clone https://github.com/scroogex/adgfilter2 /adgfilter2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /adgsearch
+  git clone $UPSTREAM_REPO /adgfilter2
 fi
-cd /adgsearch
+cd /adgfilter2
 pip3 install -U -r requirements.txt
 echo "Starting ADG Search....🔥"
 python3 bot.py
